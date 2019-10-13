63f6ab743933ff392912594b6a994d6949021d4d
kuyanov <43725417+kuyanov@users.noreply.github.com>
2019-10-13 21:31:26 +0300

    Added lengthen chain game

--- ChainField.js
+++ ChainField.js
@@ -1,47 +1,3 @@
-function segments_intersect(x1, y1, x2, y2, x3, y3, x4, y4) 
-{
-    function cross_product(x1, y1, x2, y2) {
-        return x1 * y2 - x2 * y1;
-    }
-
-    let a = cross_product(x2 - x1, y2 - y1, x3 - x1, y3 - y1) * 
-        cross_product(x2 - x1, y2 - y1, x4 - x1, y4 - y1);
-    let b = cross_product(x4 - x3, y4 - y3, x1 - x3, y1 - y3) * 
-        cross_product(x4 - x3, y4 - y3, x2 - x3, y2 - y3);
-    if (a <= 0 && b <= 0) {
-        if (a == 0 && b == 0) {
-            // оставим на потом)
-            return false;
-        }
-        return true;
-    }
-    return false;
-}
-
-function setStyles(elem, style) {
-    for (let prop in style) {
-        elem.style[prop] = style[prop]
-    }
-}
-
-function addElement(parent, tag, style, opt)
-{
-    let elem = document.createElement(tag);
-    for (let prop in opt) {
-        elem.setAttribute(prop, opt[prop]);
-    }
-    setStyles(elem, style);
-    parent.append(elem);
-    return elem;
-}
-
-function fromInterval(x, L, R) {
-    x = Math.min(x, R);
-    x = Math.max(x, L);
-    return x;
-}
-
-
 class ChainField
 {
     constructor(opt = {})
@@ -109,6 +65,7 @@ class ChainField
     static pulseNodeAnimation = pulseNodeAnimation;
 
     static cfKnightGame = cfKnightGame;
+    static cfLengthenGame = cfLengthenGame;
 
     make_busy() {
         this.busy = true;
--- games.js
+++ games.js
@@ -11,9 +11,38 @@ function cfKnightGame(x, y, table)
             return;
         }
     }
-    if ((x - table.last_point[0]) ** 2 + (y - table.last_point[1]) ** 2 != 5) {
-        ChainField.pulseNodeAnimation([256, 0, 0], 3000, 100000)(table.node(y, x));
+    let last_point = table.last_point;
+    if (squared_distance(x, y, last_point[0], last_point[1]) == 5) {
+        table.add_segment(x, y, ChainField.drawSegmentLinearAnimation);
         return;
     }
-    table.add_segment(x, y, ChainField.drawSegmentLinearAnimation);
+    ChainField.pulseNodeAnimation([256, 0, 0], 3000, 100000)(table.node(y, x));
+}
+
+function cfLengthenGame(x, y, table)
+{
+    if (table.destroy_segments(x, y, ChainField.destroySegmentLinearAnimation)) {
+        return;
+    }
+    for (let n = 1; n <= table.lines_cnt; n++) {
+        if (segments_intersect(x, y, ...table.last_point,
+            table.points[n - 1][0], table.points[n - 1][1],
+            table.points[n][0], table.points[n][1])) {
+            ChainField.pulseNodeAnimation([256, 0, 0], 3000, 100000)(table.node(y, x));
+            return;
+        }
+    }
+    if (table.lines_cnt == 0) {
+        table.add_segment(x, y, ChainField.drawSegmentLinearAnimation);
+        return;
+    }
+    let last_point = table.last_point;
+    let prelast_point = table.points[table.points.length - 2];
+    if (squared_distance(x, y, last_point[0], last_point[1]) > 
+        squared_distance(last_point[0], last_point[1], prelast_point[0], prelast_point[1]))
+    {
+        table.add_segment(x, y, ChainField.drawSegmentLinearAnimation);
+        return;
+    }
+    ChainField.pulseNodeAnimation([256, 0, 0], 3000, 100000)(table.node(y, x));
 }
