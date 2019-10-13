3d4271b097a531d8c1ba97b1051b6ef73bb17ef5
kuyanov <43725417+kuyanov@users.noreply.github.com>
2019-10-13 20:14:54 +0300

    Renamed cross to cross_product

--- ChainField.js
+++ ChainField.js
@@ -1,13 +1,13 @@
 function segments_intersect(x1, y1, x2, y2, x3, y3, x4, y4) 
 {
-    function cross(x1, y1, x2, y2) {
+    function cross_product(x1, y1, x2, y2) {
         return x1 * y2 - x2 * y1;
     }
 
-    let a = cross(x2 - x1, y2 - y1, x3 - x1, y3 - y1) * 
-        cross(x2 - x1, y2 - y1, x4 - x1, y4 - y1);
-    let b = cross(x4 - x3, y4 - y3, x1 - x3, y1 - y3) * 
-        cross(x4 - x3, y4 - y3, x2 - x3, y2 - y3);
+    let a = cross_product(x2 - x1, y2 - y1, x3 - x1, y3 - y1) * 
+        cross_product(x2 - x1, y2 - y1, x4 - x1, y4 - y1);
+    let b = cross_product(x4 - x3, y4 - y3, x1 - x3, y1 - y3) * 
+        cross_product(x4 - x3, y4 - y3, x2 - x3, y2 - y3);
     if (a <= 0 && b <= 0) {
         if (a == 0 && b == 0) {
             // оставим на потом)
